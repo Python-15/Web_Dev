a66b6f3ef0d3ab94bf6afae84aa409463c72d9f0
Aaditya <raaz.aditya15@gmail.com>
2021-08-03 01:31:35 +0530

    clone excle

--- Win11clone/scriptM.js
+++ Win11clone/scriptM.js
@@ -1,6 +1,7 @@
 
 let camera=document.getElementsByClassName("camera")[0]
 let bord=document.getElementsByClassName("Board")[0]
+let excel=document.getElementsByClassName("Excle")[0]
 
 camera.addEventListener("click", ()=>{
     window.location.assign("./camsnap/activity/index.html");
@@ -8,4 +9,7 @@ camera.addEventListener("click", ()=>{
 
 bord.addEventListener("click", ()=>{
     window.location.assign("./whiteboard/activity/index.html");
+})
+excel.addEventListener("click", ()=>{
+    window.location.assign("./Excel_sheet/index.html");
 })
